6f50ba184689c375665946d4ef904779c0abb0f3
jaime Guerrero <jaimegordon20@outlook.com>
2020-01-16 19:27:21 -0500

    rename

--- src/GoodbyeWorld.java
+++ src/GoodbyeWorld.java
@@ -1,5 +1,5 @@
 
-public class Good_byeWorld {
+public class GoodbyeWorld {
 	public static void main(String[] args) {
 	System.out.println("Goodbye, World!");
 
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 
-public class Hello {
+public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
